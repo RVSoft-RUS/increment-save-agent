eadeb7c7797af83b3ac0ec6e9f07f9308ad9fd21
e.ivanushkin <esivanushkin@sberbank.ru>
2021-03-22 18:23:54 +0300

    создал ДАО для upsert'a.

--- src/main/java/ru/sberbank/ckr/sberboard/increment/entity/IncrementStates.java
+++ src/main/java/ru/sberbank/ckr/sberboard/increment/entity/IncrementStates.java
@@ -16,29 +16,29 @@ public class IncrementStates {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column (name = "package_smd")
+    @Column(name = "package_smd")
     private String packageSmd;
-    @Column (name = "subscr_id")
+    @Column(name = "subscr_id")
     private String subscrId;
     @Column(name = "obj_type")
     private String objType;
-    @Column (name = "objs_in_pack")
+    @Column(name = "objs_in_pack")
     private Integer objsInPack;
-    @Column (name = "workflow_end_dt")
+    @Column(name = "workflow_end_dt")
     private LocalDateTime workflowEndDt;
-    @Column (name = "incr_pack_run_id")
+    @Column(name = "incr_pack_run_id")
     private Long incrPackRunId;
-    @Column (name = "start_dt")
+    @Column(name = "start_dt")
     private LocalDateTime startDt;
-    @Column (name = "end_dt")
+    @Column(name = "end_dt")
     private LocalDateTime endDt;
-    @Column (name = "target_table")
+    @Column(name = "target_table")
     private String targetTable;
     @Column(name = "min_ctl_loading")
     private Long minCtlLoading;
     @Column(name = "max_ctl_loading")
     private Long maxCtlLoading;
-    @Column (name = "incrementation_state")
+    @Column(name = "incrementation_state")
     private String incrementationState;
 }
 
