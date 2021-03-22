50824875f47e9e1de2f2286cc09cd5498efff553
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-03-22 11:04:42 +0300

    Реализовать метод получения названий полей по названию таблицы Почистил закомментированное, удалил неиспользуемые импорты

--- src/main/java/ru/sberbank/ckr/sberboard/increment/entity/IncrementStates.java
+++ src/main/java/ru/sberbank/ckr/sberboard/increment/entity/IncrementStates.java
@@ -2,12 +2,8 @@ package ru.sberbank.ckr.sberboard.increment.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.jdbc.core.RowMapper;
 
 import javax.persistence.*;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
@@ -43,38 +39,5 @@ public class IncrementStates {
     private Long maxCtlLoading;
     @Column(name = "incrementation_state")
     private String incrementationState;
-
-//    public static class IncrementStatesMapper implements RowMapper<IncrementStates> {
-//        @Override
-//        public IncrementStates mapRow(ResultSet resultSet, int i) throws SQLException {
-//            Timestamp timestamp;
-//            LocalDateTime localDateTime;
-//            IncrementStates incrementStates = new IncrementStates();
-//            incrementStates.setId(resultSet.getLong("id"));
-//            incrementStates.setPackageSmd(resultSet.getString("package_smd"));
-//            incrementStates.setSubscrId(resultSet.getString("subscr_id"));
-//            incrementStates.setObjType(resultSet.getString("obj_type"));
-//            incrementStates.setObjsInPack(resultSet.getInt("objs_in_pack"));
-//
-//            timestamp = resultSet.getTimestamp("workflow_end_dt");
-//            localDateTime = timestamp != null ? timestamp.toLocalDateTime() :  null;
-//            incrementStates.setWorkflowEndDt(localDateTime);
-//
-//            incrementStates.setIncrPackRunId(resultSet.getLong("incr_pack_run_id"));
-//
-//            timestamp = resultSet.getTimestamp("start_dt");
-//            localDateTime = timestamp != null ? timestamp.toLocalDateTime() :  null;
-//            incrementStates.setStartDt(localDateTime);
-//            timestamp = resultSet.getTimestamp("end_dt");
-//            localDateTime = timestamp != null ? timestamp.toLocalDateTime() :  null;
-//            incrementStates.setEndDt(localDateTime);
-//
-//            incrementStates.setTargetTable(resultSet.getString("target_table"));
-//            incrementStates.setMinCtlLoading(resultSet.getLong("min_ctl_loading"));
-//            incrementStates.setMaxCtlLoading(resultSet.getLong("max_ctl_loading"));
-//            incrementStates.setIncrementationState(resultSet.getString("incrementation_state"));
-//            return incrementStates;
-//        }
-//    }
 }
 
--- src/main/java/ru/sberbank/ckr/sberboard/increment/repository/IncrementStatesRepository.java
+++ src/main/java/ru/sberbank/ckr/sberboard/increment/repository/IncrementStatesRepository.java
@@ -1,13 +1,8 @@
 package ru.sberbank.ckr.sberboard.increment.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.sberbank.ckr.sberboard.increment.entity.IncrementStates;
-
-import java.util.List;
 
 @Repository
 public interface IncrementStatesRepository extends JpaRepository<IncrementStates, Long> {
