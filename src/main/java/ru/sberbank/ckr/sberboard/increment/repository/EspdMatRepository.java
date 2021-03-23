d4f24984b6e8eaf553da9503f0a69703c91d2e46
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-03-23 16:57:17 +0300

    Реализовать метод получения списка названий таблиц по каждому пакету
    1) Выбрать уникальные id подписок espd_math.subscr_id
    2) Выбрать по каждому id подписки пакет с максимальной датой и корректным статусом (espd_math.subscr_id, espd_math.package_smd, espd_math.workflow_end_dt и espd_math.espd_status = "ESPD_OK") реализовано

--- src/main/java/ru/sberbank/ckr/sberboard/increment/repository/EspdMatRepository.java
+++ src/main/java/ru/sberbank/ckr/sberboard/increment/repository/EspdMatRepository.java
@@ -14,9 +14,6 @@ public interface EspdMatRepository extends JpaRepository<EspdMat, Long> {
     @Query("SELECT DISTINCT e.subscrId FROM EspdMat e")
     List<String> findAllUniqueSubscribeId();
 
-//    @Query("SELECT e.packageSmd FROM EspdMat e WHERE e.workflowEndDt = MAX(e.workflowEndDt)")
-//    String getActualPackageToProcess(String subscrId);
-
     @Query(value = "SELECT e.package_smd, e.workflow_end_dt\n" +
             "FROM raw_data_increment.espd_mat e\n" +
             "WHERE e.subscr_id = ?1 AND e.espd_status = 'ESPD_OK'\n" +
