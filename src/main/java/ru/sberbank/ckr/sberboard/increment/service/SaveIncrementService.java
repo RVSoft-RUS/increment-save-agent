65dfcd260094ff0075434561bf0ee76da0f3db70
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-03-26 12:23:08 +0300

    Реализовать DAO получения max_ctl_loading и min_ctl_loading по названию таблицы в схеме raw_data_increment
    Не тестировалась на всех экстремальных условиях!!!

--- src/main/java/ru/sberbank/ckr/sberboard/increment/service/SaveIncrementService.java
+++ src/main/java/ru/sberbank/ckr/sberboard/increment/service/SaveIncrementService.java
@@ -65,10 +65,11 @@ public class SaveIncrementService {
                 OBJ_TYPE_PACKAGE,
                 espdMat.getWorkflowRunId()
         );
-        if (incrementStates != null && incrementStates.getIncrementationState() != null && (//todo Enum
-                incrementStates.getIncrementationState().equals("100")
-                        || incrementStates.getIncrementationState().equals("10"))
-                && incrementStates.getWorkflowEndDt().isAfter(espdMat.getWorkflowEndDt()) ) {
+        if (incrementStates != null && (//todo Enum
+                incrementStates.getIncrementationState() == 100
+                        || incrementStates.getIncrementationState() == 10)
+                && incrementStates.getWorkflowEndDt().isAfter(espdMat.getWorkflowEndDt()) )
+        {
             return null;
             }
         return espdMatObjRepository.findAllByPackageSmd(espdMat.getPackageSmd());
