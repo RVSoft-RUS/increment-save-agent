0c3cd7e0ad321ee49d2cc763f9a95e13d78bc9b7
Смирнов Роман Валентинович <RVaSmirnov@sberbank.ru>
2021-04-02 13:07:02 +0300

    SBERBOARD-1805 Создать каркас для модульных тестов

---
+++ src/test/java/ru/sberbank/ckr/sberboard/increment/service/IncrementStateServiceTest.java
@@ -0,0 +1,23 @@
+package ru.sberbank.ckr.sberboard.increment.service;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import ru.sberbank.ckr.sberboard.increment.repository.IncrementStateRepository;
+
+import static org.junit.Assert.*;
+
+public class IncrementStateServiceTest {
+    private final IncrementStateRepository repository = Mockito.mock(IncrementStateRepository.class);
+
+    @Test
+    public void saveNewIncrementStates() {
+    }
+
+    @Test
+    public void updateIncrementState() {
+    }
+
+    @Test
+    public void test1() {
+    }
+}
